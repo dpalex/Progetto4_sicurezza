ef3ddf6e2ba7c67fa3180d8690fe79f78ef3636d
dpalex <dp.alex93@gmail.com>
2017-12-07 18:48:27 +0100

    aggiunte risorse

--- src/progetto4/Progetto4.java
+++ src/progetto4/Progetto4.java
@@ -5,6 +5,9 @@
  */
 package progetto4;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 /**
  *
  * @author dp.alex
@@ -16,6 +19,46 @@ public class Progetto4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        BigInteger b = new BigInteger("4");
+        System.out.println(b);
+        System.out.println(BigInteger.probablePrime(8, new Random()));
+        System.out.println(BigInteger.probablePrime(8, new Random()));
+        System.out.println(BigInteger.probablePrime(8, new Random()));
+        System.out.println(BigInteger.probablePrime(8, new Random()));
+        System.out.println("genPrime");
+        System.out.println(genPrime());
+        System.out.println(genPrime());
+        System.out.println(genPrime());
+        randomZp();
+        randomZp();
+        randomZp();
+
     }
-    
+
+    private static BigInteger genPrime() {
+        BigInteger p = null;
+        boolean ok = false;
+        int modLength = 8;
+        int CERTAINTY = 50;
+        do {
+            p = BigInteger.probablePrime(modLength, new Random());
+            if (p.isProbablePrime(CERTAINTY)) {
+                ok = true;
+            }
+        } while (ok == false);
+        return p;
+    }
+
+    private static void randomZp() {
+        BigInteger r;
+        int modLength = 8;
+        BigInteger p = genPrime();
+        do {
+            r = new BigInteger(modLength, new Random());
+           
+        } while (r.compareTo(BigInteger.ZERO) < 0 || r.compareTo(p) >= 0);
+        
+        System.out.println("numero casuale:"+r+"  in Z:"+p);
+    }
+
 }

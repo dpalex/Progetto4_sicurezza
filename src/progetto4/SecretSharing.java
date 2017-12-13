6b6be1dedf5f6fd503253d5588a187004039d697
dpalex <dp.alex93@gmail.com>
2017-12-13 15:33:11 +0100

    client

--- src/progetto4/SecretSharing.java
+++ src/progetto4/SecretSharing.java
@@ -33,7 +33,7 @@ public class SecretSharing {
        // out.println("K -> " + k + " N - > " + n);
     }
 
-    public static Map<BigInteger, byte[]> split(byte[] secret) {
+    public  Map<BigInteger, byte[]> split(byte[] secret) {
 
         Map<BigInteger, byte[]> mapN = new TreeMap<BigInteger, byte[]>();
         BigInteger S = new BigInteger(1,secret); //rappresentazione di S
@@ -100,7 +100,7 @@ public class SecretSharing {
         return RP;
     }
     
-    public static byte[] getSecret(Map<BigInteger, byte[]> Kp){
+    public  byte[] getSecret(Map<BigInteger, byte[]> Kp){
         
         if(Kp.size()>=this.k){
             
